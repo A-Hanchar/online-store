085370bbe36a8100294bed8662543f243bc0e36d
Businator <deniullov@gmail.com>
2022-12-16 10:50:34 +0400

    feat: add footer

---
+++ src/componets/Footer/Footer.ts
@@ -0,0 +1,32 @@
+import styles from './styles/styles.css'
+
+export const Footer = () => {
+  const root = document.querySelector('#root')
+  const footer = document.createElement('footer')
+  const date = document.createElement('span')
+  const logoLink = document.createElement('a')
+  const logo = document.createElement('img')
+  const autors = document.createElement('div')
+  const businator = document.createElement('a')
+  const aHanchar = document.createElement('a')
+
+  const footerElements = [logoLink, date, autors]
+  footerElements.forEach((e) => footer.prepend(e))
+
+  const linkElemts = [logoLink, businator, aHanchar]
+  linkElemts.forEach((e) => (e.target = '_blank'))
+
+  root?.append(footer)
+  footer.classList.add(styles.footer!)
+  logoLink.href = 'https://rs.school/js/'
+  logoLink.append(logo)
+  logo.classList.add(styles.logo!)
+  logo.src = 'https://rs.school/images/rs_school_js.svg'
+  date.textContent = '2023'
+  autors.append(businator, aHanchar)
+  autors.classList.add(styles.autors!)
+  businator.href = 'https://github.com/Businator'
+  businator.textContent = 'Businator'
+  aHanchar.href = 'https://github.com/A-Hanchar'
+  aHanchar.textContent = 'A-Hanchar'
+}
