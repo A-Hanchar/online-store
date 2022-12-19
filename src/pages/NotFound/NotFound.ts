fd98698768462480c538a9145c85e28f46f62914
Businator <deniullov@gmail.com>
2022-12-19 10:08:00 +0400

    feat: add styles for page 404

--- src/pages/NotFound/NotFound.ts
+++ src/pages/NotFound/NotFound.ts
@@ -1,7 +1,14 @@
+import styles from './styles/styles.css'
+
 export const NotFound = (() => {
   const div = document.createElement('div')
 
-  div.innerHTML = '<h1>Not Found</h1>'
+  div.innerHTML = `    <div class="${styles.container!}">
+                          <div class="${styles.notFound!}"></div>
+                          <h1>Sorry, it looks like the page get lost</h1>
+                          <h2>(or someone has stolen it recently)</h2>
+                          <button class="${styles.btn!}"><a href="/" class="${styles.link!}">Back to Home</a></button>
+                        </div>`
 
   return div
 })()
