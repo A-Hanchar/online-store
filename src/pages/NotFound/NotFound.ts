7eebbf4567e0b7472e9c4949d334e8f175825ab5
Businator <deniullov@gmail.com>
2022-12-20 13:10:11 +0400

    refactor: change the way elements are created

--- src/pages/NotFound/NotFound.ts
+++ src/pages/NotFound/NotFound.ts
@@ -1,14 +1,27 @@
 import styles from './styles/styles.css'
 
 export const NotFound = (() => {
-  const div = document.createElement('div')
+  const container = document.createElement('div')
+  const notFoundImg = document.createElement('div')
+  const h1 = document.createElement('h1')
+  const h2 = document.createElement('h2')
+  const button = document.createElement('button')
+  const link = document.createElement('a')
 
-  div.innerHTML = `    <div class="${styles.container!}">
-                          <div class="${styles.notFound!}"></div>
-                          <h1>Sorry, it looks like the page get lost</h1>
-                          <h2>(or someone has stolen it recently)</h2>
-                          <button class="${styles.btn!}"><a href="/" class="${styles.link!}">Back to Home</a></button>
-                        </div>`
+  styles.container && container.classList.add(styles.container)
+  styles.notFound && notFoundImg.classList.add(styles.notFound)
+  styles.h1 && h1.classList.add(styles.h1)
+  styles.h2 && h2.classList.add(styles.h2)
+  styles.button && button.classList.add(styles.button)
+  styles.link && link.classList.add(styles.link)
 
-  return div
+  h1.textContent = 'Sorry, it looks like the page get lost'
+  h2.textContent = '(or someone has stolen it recently)'
+  link.textContent = 'Back to Home'
+  link.href = '/'
+
+  container.append(notFoundImg, h1, h2, button)
+  button.append(link)
+
+  return container
 })()
