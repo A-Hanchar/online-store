2f82f188d715b7bd2a9ea2171aa6d1a3caf65910
Artsiom Hanchar <artsiom.hanchar@gmail.com>
2022-12-20 17:08:40 +0400

    feat: update layout

--- src/components/Layout/Layout.ts
+++ src/components/Layout/Layout.ts
@@ -1,12 +1,22 @@
 import { Body } from 'components/Body'
 import { Footer } from 'components/Footer'
 import { Header } from 'components/Header'
+import { routerPathes } from 'router/routerPathes'
 import { PropsWithChildren } from 'types'
 import styles from './styles.css'
 
 export const Layout = async ({ children }: PropsWithChildren) => {
   const fragment = document.createDocumentFragment()
   const main = document.createElement('main')
+
+  switch (window.location.href) {
+    case routerPathes.home:
+      styles.mainHome && main.classList.add(styles.mainHome)
+      break
+
+    default:
+      break
+  }
 
   if (children) {
     const content = await children()
