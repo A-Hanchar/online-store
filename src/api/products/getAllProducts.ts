dbaa18bf6b906725840e95d1ffa3c6647ee8d12d
Artsiom Hanchar <artsiom.hanchar@gmail.com>
2022-12-21 20:24:38 +0400

    feat: update request

--- src/api/products/getAllProducts.ts
+++ src/api/products/getAllProducts.ts
@@ -5,7 +5,7 @@ import { products } from './endpoints'
 export const getAllProducts = ({ page, searchParams: { limit = 20 } }: AllProductsRequestParams) => {
   const skip = page * limit
 
-  const requestUrl = `${products}?limit=${limit}&skip=${skip}&category==groceries`
+  const requestUrl = `${products}?limit=${limit}&skip=${skip}`
 
   return fetch(requestUrl).then((response): Promise<IProductsResponse> => response.json())
 }
