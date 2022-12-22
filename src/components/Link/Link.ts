4b38c3135377d66090be8497f2a10fd6dd977216
Businator <deniullov@gmail.com>
2022-12-22 20:13:50 +0400

    feat: add target prop in link functiom

--- src/components/Link/Link.ts
+++ src/components/Link/Link.ts
@@ -2,16 +2,20 @@ import { addClassnameToElement } from 'helpers/addClassnameToElement'
 import { renderComponent } from 'router'
 import { LinkProps } from './types'
 
-export const Link = ({ id, children, href, classname }: LinkProps) => {
+export const Link = ({ id, children, href, classname, target = '_self' }: LinkProps) => {
   const a = document.createElement('a')
+  children && a.append(children)
 
   a.id = id
   a.href = href
+  a.target = target
 
-  children && a.append(children)
   addClassnameToElement({ element: a, classname })
 
   const handleLinkClick = (event: MouseEvent) => {
+    if (target === '_blank') {
+      return
+    }
     event.preventDefault()
 
     window.history.pushState({}, '', href)
