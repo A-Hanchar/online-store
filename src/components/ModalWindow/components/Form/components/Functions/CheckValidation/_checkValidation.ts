dee13c656dba949d71cd97c3aefcca279251d4ac
Businator <deniullov@gmail.com>
2022-12-23 16:30:26 +0400

    refactor: add enum for checkvalidation function

--- src/components/ModalWindow/components/Form/components/Functions/CheckValidation/_checkValidation.ts
+++ src/components/ModalWindow/components/Form/components/Functions/CheckValidation/_checkValidation.ts
@@ -3,6 +3,7 @@ import { addClassnameToElement, createElementWithClassName } from 'helpers'
 import { regArr } from './../../RegExp/RegExp'
 import styles from './styles.css'
 import { removeClassnameToElement } from 'helpers/removeClassnameToElement '
+import { ID_FOR_REGEXP } from './enum'
 
 export const checkValidation = (form: HTMLFormElement, arr: typeof regArr) => {
   const allInputs = form.querySelectorAll('input')
@@ -27,7 +28,7 @@ export const checkValidation = (form: HTMLFormElement, arr: typeof regArr) => {
 
   for (const input of allInputs) {
     arr.forEach((e) => {
-      if (input.dataset.name && e.id === 'name') {
+      if (input.dataset.name && e.id === ID_FOR_REGEXP.name) {
         if (e.regExp.test(input.value)) {
           removeError(input)
         } else {
@@ -35,7 +36,7 @@ export const checkValidation = (form: HTMLFormElement, arr: typeof regArr) => {
           res = false
         }
       }
-      if (input.dataset.phone && e.id === 'phone') {
+      if (input.dataset.phone && e.id === ID_FOR_REGEXP.phone) {
         if (e.regExp.test(input.value)) {
           removeError(input)
         } else {
@@ -43,7 +44,7 @@ export const checkValidation = (form: HTMLFormElement, arr: typeof regArr) => {
           res = false
         }
       }
-      if (input.dataset.address && e.id === 'address') {
+      if (input.dataset.address && e.id === ID_FOR_REGEXP.address) {
         if (e.regExp.test(input.value)) {
           removeError(input)
         } else {
@@ -51,7 +52,7 @@ export const checkValidation = (form: HTMLFormElement, arr: typeof regArr) => {
           res = false
         }
       }
-      if (input.dataset.email && e.id === 'email') {
+      if (input.dataset.email && e.id === ID_FOR_REGEXP.email) {
         if (e.regExp.test(input.value)) {
           removeError(input)
         } else {
@@ -59,7 +60,7 @@ export const checkValidation = (form: HTMLFormElement, arr: typeof regArr) => {
           res = false
         }
       }
-      if (input.dataset.number && e.id === 'number') {
+      if (input.dataset.number && e.id === ID_FOR_REGEXP.number) {
         if (e.regExp.test(input.value)) {
           removeError(input)
         } else {
@@ -67,7 +68,7 @@ export const checkValidation = (form: HTMLFormElement, arr: typeof regArr) => {
           res = false
         }
       }
-      if (input.dataset.valid && e.id === 'valid') {
+      if (input.dataset.valid && e.id === ID_FOR_REGEXP.valid) {
         if (e.regExp.test(input.value)) {
           removeError(input)
         } else {
@@ -75,7 +76,7 @@ export const checkValidation = (form: HTMLFormElement, arr: typeof regArr) => {
           res = false
         }
       }
-      if (input.dataset.code && e.id === 'code') {
+      if (input.dataset.code && e.id === ID_FOR_REGEXP.code) {
         if (e.regExp.test(input.value)) {
           removeError(input)
         } else {
---
+++ src/components/ModalWindow/components/Form/components/Functions/CheckValidation/enum.ts
@@ -0,0 +1,9 @@
+export enum ID_FOR_REGEXP {
+  name = 'name',
+  phone = 'phone',
+  address = 'address',
+  email = 'email',
+  number = 'number',
+  valid = 'valid',
+  code = 'code',
+}
