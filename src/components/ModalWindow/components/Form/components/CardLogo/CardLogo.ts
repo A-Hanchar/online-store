9257ac55d47c608f6c2a34726f2daff0a8ca5842
Businator <deniullov@gmail.com>
2022-12-23 16:05:04 +0400

    refactor: change cardlogo function

--- src/components/ModalWindow/components/Form/components/CardLogo/CardLogo.ts
+++ src/components/ModalWindow/components/Form/components/CardLogo/CardLogo.ts
@@ -1,10 +1,12 @@
-import { createElementWithClassName } from 'helpers'
 import styles from './styles.css'
+import { Image } from 'components/Image'
 
 export const CardLogo = () => {
-  const cardLogo = createElementWithClassName({ tagName: 'img', classname: styles.logo })
-  cardLogo.src =
-    'https://i.guim.co.uk/img/media/b73cc57cb1d46ae742efd06b6c58805e8600d482/16_0_2443_1466/master/2443.jpg?width=445&quality=85&dpr=1&s=none'
+  const cardLogo = Image({
+    src: 'https://i.guim.co.uk/img/media/b73cc57cb1d46ae742efd06b6c58805e8600d482/16_0_2443_1466/master/2443.jpg?width=445&quality=85&dpr=1&s=none',
+    alt: 'Logo Bank',
+    classname: styles.logo,
+  })
 
   return cardLogo
 }
