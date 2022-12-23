11dbb8cee132130b92afc6e894a766be8e0e23cd
Businator <deniullov@gmail.com>
2022-12-23 16:24:56 +0400

    refactor: change cardInput function, add switch case

--- src/components/ModalWindow/components/Form/components/CardInput/CardInput.ts
+++ src/components/ModalWindow/components/Form/components/CardInput/CardInput.ts
@@ -37,19 +37,21 @@ export const CardInput = () => {
   const cardLogo = CardLogo()
 
   const num = cardNumber.querySelector('input')
-
   num!.addEventListener('input', () => {
-    cardLogo.src =
-      'https://i.guim.co.uk/img/media/b73cc57cb1d46ae742efd06b6c58805e8600d482/16_0_2443_1466/master/2443.jpg?width=445&quality=85&dpr=1&s=none'
-    if (num!.value.startsWith('2')) {
-      cardLogo.src = 'https://upload.wikimedia.org/wikipedia/commons/b/b9/Mir-logo.SVG.svg'
-    }
-    if (num!.value.startsWith('4')) {
-      cardLogo.src = 'https://upload.wikimedia.org/wikipedia/commons/d/d6/Visa_2021.svg'
-    }
-    if (num!.value.startsWith('5')) {
-      cardLogo.src =
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/MasterCard_Logo.svg/300px-MasterCard_Logo.svg.png?20140711182052'
+    switch (num!.value.startsWith('')) {
+      case num!.value.startsWith('2'):
+        cardLogo.src = 'https://upload.wikimedia.org/wikipedia/commons/b/b9/Mir-logo.SVG.svg'
+        break
+      case num!.value.startsWith('4'):
+        cardLogo.src = 'https://upload.wikimedia.org/wikipedia/commons/d/d6/Visa_2021.svg'
+        break
+      case num!.value.startsWith('5'):
+        cardLogo.src = 'https://upload.wikimedia.org/wikipedia/commons/d/d6/Visa_2021.svg'
+        break
+      default:
+        cardLogo.src =
+          'https://i.guim.co.uk/img/media/b73cc57cb1d46ae742efd06b6c58805e8600d482/16_0_2443_1466/master/2443.jpg?width=445&quality=85&dpr=1&s=none'
+        break
     }
   })
 
