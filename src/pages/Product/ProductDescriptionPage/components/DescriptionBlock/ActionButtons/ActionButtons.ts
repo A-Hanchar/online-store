79a685ad31a1994c8a17432226e7d1f4aa14eeda
Businator <deniullov@gmail.com>
2022-12-29 16:51:46 +0400

    feat: add product to cart and go to cart page

--- src/pages/Product/ProductDescriptionPage/components/DescriptionBlock/ActionButtons/ActionButtons.ts
+++ src/pages/Product/ProductDescriptionPage/components/DescriptionBlock/ActionButtons/ActionButtons.ts
@@ -8,14 +8,21 @@ import { AddToCartButton } from 'components/ProductCard/components/ActionButtons
 export const ActionButtons = ({ id, category, brand, price, discount }: ActionButtonsProps) => {
   const wrapper = createElementWithClassName({ tagName: 'div', classname: styles.actionButtons })
 
-  wrapper.append(
-    Link({
-      id: `${category}-${brand}-${id}`,
-      children: 'Buy Now',
-      href: localStorageInstanse.hasProductId(id) ? '#' : routerPathes.basket,
-      classname: styles.buyButton,
-    }),
-    AddToCartButton({ id, textInBasket: '-', textNotInBasket: '+', price, discount }),
-  )
+  const link = Link({
+    id: `${category}-${brand}-${id}`,
+    children: 'Buy Now',
+    href: localStorageInstanse.hasProductId(id) ? '#' : routerPathes.basket,
+    classname: styles.buyButton,
+  })
+
+  link.addEventListener('click', () => {
+    if (!localStorageInstanse.hasProductId(id)) {
+      link.href = routerPathes.basket
+      localStorageInstanse.setProduct({ id, count: 1, price, discount })
+      localStorageInstanse.updateBasket()
+    }
+  })
+
+  wrapper.append(link, AddToCartButton({ id, textInBasket: '-', textNotInBasket: '+', price, discount }))
   return wrapper
 }
